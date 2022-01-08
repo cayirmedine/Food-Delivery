158d933877aa2c744836ce0d8de5d56ac999ec4c
Medine Çayır <htc.mdn.2010@gmail.com>
2022-01-08 11:35:24 +0300

    sequelize.sync function added to index router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+const { sequelize } = require('../database/db');
+let router = express.Router();
+
+sequelize.sync({ force: false }).then((result) => {
+  console.log("Connected DB");
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
