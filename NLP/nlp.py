6583f556daecec960e0d2bd9cb61e2d2bb108c55
Medine Çayır <htc.mdn.2010@gmail.com>
2022-01-19 14:30:40 +0300

    nlp file added

---
+++ NLP/nlp.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import sys
+
+x = sys.argv[0]
+
+tokenizer = AutoTokenizer.from_pretrained(
+    'nlptown/bert-base-multilingual-uncased-sentiment')
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    'nlptown/bert-base-multilingual-uncased-sentiment')
+
+
+def sentiment_score(review):
+    tokens = tokenizer.encode(review, return_tensors='pt')
+    result = model(tokens)
+    return int(torch.argmax(result.logits))+1
+
+print(sentiment_score(x[:100]))
